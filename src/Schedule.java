1c7df99b5299a7b812602a723600aa280251ba8e
Pavel <pavel-sbor@rambler.ru>
2015-02-10 19:30:07 +0300

    C2

--- src/Schedule.java
+++ src/Schedule.java
@@ -7,7 +7,7 @@ public class Schedule {
         //SimpleDateFormat df = new SimpleDateFormat("dd.MM.YYYY-HH:mm:ss");
         GregorianCalendar dc = new GregorianCalendar(2015,1,10,20,23,15);
         User u = new User("pasha", TimeZone.getTimeZone(("Europe/Moscow")), true);
-        u.AddEvent(dc.getTime(), "Hello world!");
+        u.AddEvent(dc.getTime(), "Hello worlds!");
         u.ShowInfo();
     }
 }
