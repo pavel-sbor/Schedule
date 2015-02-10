00627f24c5c40c76f7295b428baf0d62702afd4d
Pavel <pavel-sbor@rambler.ru>
2015-02-10 19:26:36 +0300

    C2

--- src/Schedule.java
+++ src/Schedule.java
@@ -1,16 +1,11 @@
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.TimeZone;
 
-/**
- * Created by Pavel on 09.02.2015.
- */
 public class Schedule {
     public static void main(String[] args)
     {
-        SimpleDateFormat df = new SimpleDateFormat("dd.MM.YYYY-HH:mm:ss");
-        GregorianCalendar dc = new GregorianCalendar(2015,1,10,12,40,30);
+        //SimpleDateFormat df = new SimpleDateFormat("dd.MM.YYYY-HH:mm:ss");
+        GregorianCalendar dc = new GregorianCalendar(2015,1,10,20,23,15);
         User u = new User("pasha", TimeZone.getTimeZone(("Europe/Moscow")), true);
         u.AddEvent(dc.getTime(), "Hello world!");
         u.ShowInfo();
